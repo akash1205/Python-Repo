8fb1183143e6099f4972c0a4c4c15a430c02de99
Akash <jain2ar@mail.uc.edu>
2017-03-15 20:27:49 -0400

    Commit 3

--- Test3.py
+++ Test3.py
@@ -1,4 +1,4 @@
-filehand=open("/Users/akashjain/Google Drive/Workplace/Skills/Python/hey.txt")
+filehand=open("/Users/akashjain/Google Drive/Workplace/Skills/Python/Python-Repo/hey.txt")
 count = 0
 l=list()
 for line in filehand:
@@ -10,7 +10,7 @@ for line in filehand:
 print(l)
 filehand.close()
 
-filehand=open("/Users/akashjain/Google Drive/Workplace/Skills/Python/hey.txt")
+filehand=open("/Users/akashjain/Google Drive/Workplace/Skills/Python/Python-Repo/hey.txt")
 d = dict()
 for line in filehand:
     x=line.split()
@@ -21,12 +21,14 @@ for line in filehand:
             d[words] = d[words] + 1
 
 print(d)
-filehand.close()
-
+#filehand.close()
+filehand.seek(0)
+a=filehand.readlines()
+print(a[1])
 s= "akashjain"
 p=list(s)
 print(p)
 
-hand1 = open("/Users/akashjain/Google Drive/Workplace/Skills/Python/hey1.txt", 'w')
+hand1 = open("/Users/akashjain/Google Drive/Workplace/Skills/Python/Python-Repo/hey1.txt", 'w')
 hand1.write("test123")
 hand1.close()
---
+++ test2.py
@@ -0,0 +1,5 @@
+for i in range(1,11,2):
+    print (((".|.")*i).center(33,"-"))
+print (("Welcome").center(33,'-'))
+for i in range(9,-1,-2):
+    print (((".|.")*i).center(33,"-"))
